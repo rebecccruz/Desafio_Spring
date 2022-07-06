672c4215c768421600fc9c89d54edd3427726525
laridevmeli <larissa.navarro@mercadolivre.com>
2022-07-06 10:59:28 -0300

    main:criacao do projeto e branch development e ajuste de pacotes

--- desafio-spring/src/main/java/com/github/transformeli/desafiospring/DesafioSpringApplication.java
+++ desafio-spring/src/main/java/com/github/transformeli/desafiospring/DesafioSpringApplication.java
@@ -1,4 +1,4 @@
-package com.example.desafiospring;
+package com.github.transformeli.desafiospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- desafio-spring/src/test/java/com/github/transformeli/desafiospring/DesafioSpringApplicationTests.java
+++ desafio-spring/src/test/java/com/github/transformeli/desafiospring/DesafioSpringApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.desafiospring;
+package com.github.transformeli.desafiospring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
