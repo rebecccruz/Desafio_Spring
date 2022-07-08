c923ccd484674f816975cb00e25af0c1c54b2338
Isaias <isaias.finger@mercadolivre.com>
2022-07-08 12:26:27 -0300

    feat: post createArticles now working

--- src/main/java/com/github/transformeli/desafiospring/controller/ArticlesController.java
+++ src/main/java/com/github/transformeli/desafiospring/controller/ArticlesController.java
@@ -8,12 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -48,8 +43,7 @@ public class ArticlesController {
 
     @PostMapping("/insert-articles-request")
     public ResponseEntity<List<ProductDTO>> createArticles(@RequestBody List<Product> articleList) {
-        List<ProductDTO> productDtoList = new ArrayList<>();
-        articleList.stream().forEach(System.out::println);
-        return new ResponseEntity(productDtoList, HttpStatus.CREATED);
+        articleList.stream().forEach(product -> service.saveProduct(product));
+        return new ResponseEntity(service.getAllArticles(articleList), HttpStatus.CREATED);
     }
 }
