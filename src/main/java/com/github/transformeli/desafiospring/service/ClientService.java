d6af4a9866eb09e8d79a9d2b48ce8fccae26bcb9
evycoliveira <evelyn.oliveira@mercadolivre.com>
2022-07-10 20:07:08 -0300

    Add testes de requisicao para Clientes * Familia 2xx, 4xx e 5xx

--- src/main/java/com/github/transformeli/desafiospring/service/ClientService.java
+++ src/main/java/com/github/transformeli/desafiospring/service/ClientService.java
@@ -1,13 +1,16 @@
 package com.github.transformeli.desafiospring.service;
 
 import com.github.transformeli.desafiospring.dto.ClientDTO;
+import com.github.transformeli.desafiospring.exception.BadRequestException;
 import com.github.transformeli.desafiospring.exception.ClientExistsException;
 import com.github.transformeli.desafiospring.exception.InvalidClientParamException;
 import com.github.transformeli.desafiospring.helper.CPFDocument;
+import com.github.transformeli.desafiospring.helper.ClientDocument;
 import com.github.transformeli.desafiospring.model.Client;
 import com.github.transformeli.desafiospring.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -55,28 +58,38 @@ public class ClientService implements IClientService {
     }
     /**
      * This method check if attributes is empty and call exceptions
-     * @author Alexandre Borges
+     * @author Alexandre Borges and Evelyn Cristini Oliveira
      * @param client client object
      */
     private void validateAddNewClient ( Client client) {
         client.setCpf(CPFDocument.getNumberOnlyCPF(client.getCpf()));
+        client.setName(ClientDocument.getOnlyName(client.getName()));
+
         String[] arrClientName = client.getName().split("\\s");
+        String[] arrClientState = client.getState().split("\\s");
+        String[] arrClientCountry = client.getCountry().split("\\s");
+
         if (client.getName().isEmpty()) {
             throw new InvalidClientParamException("Client name is required");
         } else if (arrClientName.length < 2) {
             throw new InvalidClientParamException("Client requires full name");
+        } else if (!ClientDocument.isValidClient(client.getName())) {
+            throw new BadRequestException("Client name is not valid. Client name can contain only uppercase and lowercase letters");
         }
+
         if (client.getCountry().isEmpty()) {
             throw new InvalidClientParamException("Client country is required");
         }
+
         if (client.getState().isEmpty()) {
             throw new InvalidClientParamException("Client state is required");
         }
+
         if (client.getCpf().isEmpty()) {
             throw new InvalidClientParamException("Client CPF is required");
         }
-        else if (!CPFDocument.isValideCPF(client.getCpf())) {
-            throw new InvalidClientParamException("Client CPF is not valide. CPF:" + client.getCpf());
+        else if (!CPFDocument.isValidCPF(client.getCpf())) {
+            throw new BadRequestException("Client CPF is not valid. CPF:" + client.getCpf());
         }
         else if (this.getClientByCPF(client)) {
             throw new ClientExistsException("Client create request (" + client.getCpf() + ") already exists.");
